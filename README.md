0d57036722177a3e1cffcd4ade240e96e7d8a888
Rahul Krishna <i.m.ralk@gmail.com>
2020-04-10 21:30:44 -0400

    Autocommit

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 Frisky Frog
 -----------
+[![Build Status](https://travis-ci.org/ai-se/se4sci.svg?branch=master)](https://travis-ci.org/ai-se/se4sci)
+[![Coverage Status](https://coveralls.io/repos/github/se4sci/defect-prediction/badge.svg?branch=master)](https://coveralls.io/github/se4sci/defect-prediction?branch=master)
+![Python Version](https://img.shields.io/badge/python-3.7-blue)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+```
 
                              .-----.
                             /7  .  (
@@ -31,3 +36,4 @@ Frisky Frog
  .-._.-'     '"  ) .-'   `.                   (  '"     `-._.--.
 (_________.-====' / .' /\_)`--..__________..-- `====-. _________)
                  (.'(.'
+```
---
+++ src/gh_crawler.py
@@ -0,0 +1,57 @@
+import requests
+
+class GHArchiveCrawler:
+    def __init__(self, hour, day, month, year):
+    """
+    A crawler for GH Archive (https://www.gharchive.org/).
+
+    Query GH Archive and covert data in to a format that can be processed with pandas, csv, etc.
+
+    Parameters
+    ----------
+    hour: Tuple(Int, Int) (default=(0, 23))
+        Hourly data as a tuple of start and end values.
+    day: Tuple(Int, Int) (default=(01, 31))
+        Daily data as a tuple of start and end values.
+    month: Tuple(Int, Int) (default=(01, 12))
+        Monthly data as a tuple of start and end values.
+    year: Tuple(Int, Int) (default=(2019, 2020))
+        Yearly data as a tuple of start and end values.
+
+    Notes
+    -----    
+    + For specific values use Discrete numbers.
+      E.g., for April, 20th 2019, 4:00 AM -- 5:00 AM provide: hour=4, day=20, month=4, year=2019.
+      
+    + For ranges provide a tuple of start and end values
+      E.g., for first half of 2019 provide: hour=(0, 23), day=(1, 31), month=(1, 6), year=2019.
+    """
+
+    self.day = 
+    self.year = 
+    self.hour = 
+    self.month = 
+
+def datarange2url():
+    """
+    Converts user provided time into a github archive url.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
+
+def url2json(gh_archive_string):
+    """
+    Generates a json file with all the metadata
+    """
+
+def json2dataframe(json):
+    """
+
+    """
+if __name__ == "__main__":
+    
