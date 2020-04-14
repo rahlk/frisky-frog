1955000250491c1ee1a26786d8d840f803acb889
Rahul Krishna <i.m.ralk@gmail.com>
2020-04-14 17:17:46 -0400

    Autocommit

---
+++ tests/test_agglomerate.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import numpy
+import unittest
+from pathlib import Path
+from pdb import set_trace
+
+# Add project source to path
+root = Path(os.path.abspath(os.path.join(
+    os.getcwd().split("frisky-frog")[0], 'frisky-frog')))
+
+if root.joinpath('src') not in sys.path:
+    sys.path.append(str(root.joinpath('src')))
+
+from crawler import Agglomerate
+
+
+class TestCrawler(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestCrawler, self).__init__(*args, **kwargs)
+        self.save_path = Path(os.path.abspath(os.getcwd())).joinpath('tests')
+        self.crawl = Crawler(
+            hour=(16), date=(12, 13), month=(3), year=2020)
+
+    def test_daterange2url(self):
+        count = 0
+        for url in self.crawl._daterange2url():
+            count += 1
+        self.assertEqual(count, 2)
+
+    def test_get_events_as_dataframe(self):
+        dataframe = self.crawl.get_events_as_dataframe()
+
+    def test_save_events_as_json(self):
+        dataframe = self.crawl.save_events_as_json(save_path=self.save_path)
+
+    def test_save_events_as_csv(self):
+        dataframe = self.crawl.save_events_as_csv(save_path=self.save_path)
