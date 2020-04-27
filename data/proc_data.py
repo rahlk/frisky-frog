9e5e15be4c14764395658051611dcf04716954c8
Rahul Krishna <i.m.ralk@gmail.com>
2020-04-27 14:17:00 -0400

    Autocommit

---
+++ data/proc_data.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import json
+import pandas
+from pathlib import Path
+
+# -- Update paths --
+# - Logging Config -
+logging.basicConfig(format='[+] %(message)s', level=logging.INFO)
+
+# - Add project source to path -
+root = Path(os.path.abspath(os.path.join(
+    os.getcwd().split('frisky-frog')[0], 'frisky-frog')))
+
+# - Add data dir to path -
+data = root.joinpath('data')
+
+
+if root.joinpath('src') not in sys.path:
+    sys.path.append(str(root.joinpath('src')))
+
+
+def measure_json2table():
+    for file in data.joinpath('measures', 'repositories', 'all'):
+        with open(file, "r") as json_file:
+            set_trace()
+        
+
